448223b2411b103086546382e100cb88eb469d0c
Adil Sari <sari.helfi@gmail.com>
2024-08-12 16:39:23 +0200

    change desc validations with regex, tested ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,14 +2,5 @@ class Review < ApplicationRecord
   belongs_to :event
 
   validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :comment, length: { minimum: 5 }, too_short: "doit contenir au moins %{count} caractères"
-  validates :comment_cannot_be_blank_or_whitespace
-
-  private
-
-  def comment_cannot_be_blank_or_whitespace
-    if comment.strip.empty?
-      errors.add(:comment, "cannot be blank or consist only of whitespace")
-    end
-  end
+  validates :comment , length: { minimum: 5 }, format: { with: /\A\S.*\S\z|\A\S\z/, message: "cannot be blank or consist only of whitespace" }
 end
