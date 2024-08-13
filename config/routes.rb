0b384a2a8d82e5657a36de5e06e171fbf6da0473
Xinhe YU <aspasia.yu@gmail.com>
2024-08-13 12:38:26 +0200

    improve fighter's create/edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/dashboard', to: "pages#dashboard"
 
   resources :fighters, only: %i[new create edit update destroy]
+  resources :events
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
