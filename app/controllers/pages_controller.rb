e9ab986ae6dff63e1650e6569af3bb2266014eab
Xinhe YU <aspasia.yu@gmail.com>
2024-08-13 17:58:52 +0200

    finish today's work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,23 @@ class PagesController < ApplicationController
 
   def dashboard
     @events = Event.where(user_id: current_user.id)
+    if current_user.fighter
+      fighter_events = Event.where(fighter_id: current_user.fighter.id)
+      @fighter_events = fighter_events.empty? ? nil : [pending_events(fighter_events), accepted_events(fighter_events)]
+    else
+      @fighter_events = nil
+    end
+  end
+
+  private
+
+  def pending_events(fighter_events)
+    pending_events = fighter_events.where(status: "pending")
+    pending_events.empty? ? nil : pending_events
+  end
+
+  def accepted_events(fighter_events)
+    accepted_events = fighter_events.where(status: "accepted")
+    accepted_events.empty? ? nil : accepted_events
   end
 end
