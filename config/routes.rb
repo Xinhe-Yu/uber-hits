38e1881e808a7670eb67676a3fb3ae5f46d4f191
Xinhe YU <aspasia.yu@gmail.com>
2024-08-13 11:51:52 +0200

    fighter edit views

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#home"
   get '/dashboard', to: "pages#dashboard"
-  resources :fighters
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :fighters, only: %i[new create edit update destroy]
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
