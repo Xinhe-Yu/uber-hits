7d617e5c48f925de3ac727b78eb634394192a52f
Severine CELIGNY <severine.celigny@gmail.com>
2024-08-14 11:44:22 +0200

    Integration flatpickr in event creation

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,9 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
-
+import rangePlugin from "flatpickrRangePlugin";
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static targets = ["startTimeInput", "endTimeInput"]
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.startTimeInputTarget, {
+      "plugins": [new rangePlugin({ input: this.endTimeInputTarget})],
+      enableTime: true,
+
+      })
   }
 }
