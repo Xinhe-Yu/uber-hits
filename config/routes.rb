73fb67ff25d68c6a58c05263ce8ca40362ca2c1e
Xinhe YU <aspasia.yu@gmail.com>
2024-08-14 15:28:48 +0200

    add availability routes and controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
   get '/dashboard', to: "pages#dashboard"
+  get '/planning', to: "pages#planning"
   resources :fighters do
     resources :events, only: %i[new create]
   end
