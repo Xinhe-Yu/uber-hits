4ef5b7c92f2c5b4d8b45e7bda4681370791b7fdb
Xinhe YU <aspasia.yu@gmail.com>
2024-08-15 20:41:12 +0200

    refact fighter's validation : description + birth date

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -19,17 +19,17 @@ class Fighter < ApplicationRecord
   validates :nickname, uniqueness: true
   validates :birth_date, presence: true
   validate :birth_date_cannot_be_in_the_future
-  validates :height, numericality: {
+  validates :height, presence: true, numericality: {
     greater_than_or_equal_to: 120,
     less_than_or_equal_to: 300,
     message: "must be between 120 and 300 cm"
   }
-  validates :weight, numericality: {
+  validates :weight, presence: true, numericality: {
     greater_than_or_equal_to: 45,
     less_than_or_equal_to: 200,
     message: "must be between 45 and 200 kg"
   }
-  validate :cannot_have_only_whitespace
+  validate :validate_description
 
   # Calculate average rating for the fighter
   def average_rating
@@ -48,16 +48,17 @@ class Fighter < ApplicationRecord
 
   private
 
-  def cannot_have_only_whitespace
-    if description.length.positive? && !description.match?(/\S/)
-      errors.add(:description, "cannot consist only of whitespaces")
-      return false
-    end
+  def validate_description
+    validate_field_cannot_have_only_whitespace(:description)
   end
 
   def birth_date_cannot_be_in_the_future
-    if birth_date.present? && birth_date > Date.today
+    if birth_date > Date.today
       errors.add(:birth_date, "can't be in the future")
+    elsif birth_date > Date.today - 14.years
+      errors.add(:birth_date, "can't be less than 14 years old")
+    elsif birth_date < Date.today - 120.years
+      errors.add(:birth_date, "can't be more than 120 years old")
     end
   end
 
