bc9ec94c128be3f60512b32e2533f1a8979bd47e
Xinhe YU <aspasia.yu@gmail.com>
2024-08-15 20:27:06 +0200

    add 'validate_field_cannot_have_only_whitespace' callback function in ApplicationRecord class

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def validate_field_cannot_have_only_whitespace(field_name, min_length: 3)
+    value = send(field_name)
+    return unless value.length.positive?
+
+    errors.add(field_name, "cannot consist only of whitespaces") unless value.match?(/\S/)
+    errors.add(field_name, "cannot be less than #{min_length} characters") if value.length < 3
+  end
 end
